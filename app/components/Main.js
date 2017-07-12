15e8444dcf2bc0da14d01779f567c1d0e97fd845
Andreas <AndreasPapaz@gmail.com>
2017-07-12 00:46:29 -0500

    more updates for Main/Login

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react';
 import Login from './Login';
-// import GetService from './utils/getService';
+import GetService from './utils/getService';
 
-// let verifyService = new GetService('/api/verify/');
+let verifyService = new GetService('/api/verify/');
 
 export default class Main extends Component {
   getInitialState() {
     return {}
   }
-  // componentWillMount() {
-  //   verifyService.getRoute().then(function(res) {
-  //     this.setState({
-  //       auth: res.auth;
-  //       googleId: res.googleId
-  //     });
-  //   }.bind(this));
-  // }
+  componentWillMount() {
+    verifyService.getRoute().then(function(response) {
+            this.setState({
+                auth: response.auth,
+                googleId: response.googleId
+            });
+        }.bind(this));
+  }
   render() {
     return(
       <div className='ui container'>
