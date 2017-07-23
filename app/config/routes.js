0dee033d4a6201dbc94dc8ad84143c8fa2839fe7
Andreas <AndreasPapaz@gmail.com>
2017-07-23 17:33:36 -0500

    working with local storage/ i would like to get express sessions working but im having difficulties

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory, Switch, Redirect } from 'react-router';
 
 import AuthContainer from '../components/AuthContainer';
 import LandingPage from '../components/LandingPage';
-import App2 from '../components/App2';
 import Main from '../components/Main';
 import LoginPage from '../components/children/register/LoginPage';
 import SignUpPage from '../components/children/register/SignUpPage';
@@ -38,6 +37,7 @@ export default (
 			<Route path='login' component={LoginPage}>
 				<Route path='signup' component={SignUpPage} />
 			</Route>
+			<Route path='dashboard' component={Dashboard} />
 			<Route path='signup' component={SignUpPage} />
 			<IndexRoute component={LoginPage} />
 		</Route>
@@ -45,16 +45,22 @@ export default (
 
 );
 
-// export default (
 
-// <Route path='/' component={App2}>
-// 	<Route path='login' component={LoginPage} />
-// 	<Route path='signup' component={SignUpPage} />
+// export class routes extends React.Component {
+// 	render() {
+// 		return (
+// 		<Route path='/' component={App2}>
+// 			<Route path='login' component={LoginPage} />
+// 			<Route path='signup' component={SignUpPage} />
 
-// 	<Route component={AuthContainer}>
-// 		<Route path='dashboard' component={dashboard} />
-// 		<Route path='test' component={test} />
-// 	</Route>
-// </Route>
+// 			<Route component={AuthContainer}>
+// 				<Route path='dashboard' component={Dashboard} />
+// 				<Route path='test' component={test} />
+// 			</Route>
+// 		</Route>
+// 	)
+// 	}
+// }
 
-// );
+
+
